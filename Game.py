355e83d48cc89bcd3cb400647ff1b76e5c14b081
Eduardo Robles <robles5@pdx.edu>
2019-05-15 12:32:01 -0700

    Added player movement

--- Game.py
+++ Game.py
@@ -8,16 +8,39 @@ class Game:
         pygame.init()
         self.SCREEN_WIDTH = 800
         self.SCREEN_HEIGHT = 600
+        self.BACKGROUND_COLOR = (0,0,0) #black color
+        self.PLAYER_COLOR = (0,0,255) #blue color
+        self.PLAYER_SIZE = 50
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
-        self.BACKGROUND_COLOR = (0,0,0)
+        self.player_pos = [self.SCREEN_WIDTH/2, self.SCREEN_HEIGHT-2*self.PLAYER_SIZE]
         self.game_over = False
+        
 
     def run(self):
         while not self.game_over:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                
+                if event.type == pygame.KEYDOWN:
+                    x_coordinate = self.player_pos[0]
+                    y_coordinate = self.player_pos[1]
+
+                    if event.key == pygame.K_LEFT:
+                        x_coordinate -= self.PLAYER_SIZE
+                        if(x_coordinate < 0):
+                            x_coordinate = 0
+
+                    elif event.key == pygame.K_RIGHT:
+                        x_coordinate += self.PLAYER_SIZE
+                        if(x_coordinate > self.SCREEN_WIDTH-50):
+                            x_coordinate = self.SCREEN_WIDTH-50
+
+                    self.player_pos = [x_coordinate,y_coordinate]
 
             self.screen.fill(self.BACKGROUND_COLOR)
+            pygame.draw.rect(self.screen, self.PLAYER_COLOR, (self.player_pos[0], self.player_pos[1], self.PLAYER_SIZE, self.PLAYER_SIZE))
             pygame.display.update()
+
+
 
